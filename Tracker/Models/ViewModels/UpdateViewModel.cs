7c357f60edd7a2e9206feb439f704a3eef5bb78e
NoorN <NoorN@NOOR>
2025-02-03 13:44:01 +0300

    profile-edite

---
+++ Tracker/Models/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class UpdateViewModel
+{
+    [Required(ErrorMessage = "Enter Your Name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Enter Your Email")]
+    [EmailAddress]
+    [MinLength(6, ErrorMessage = "Invalid email address")]
+    public string Email { get; set; }
+
+    [Display(Name = "Profile Picture")]
+    public IFormFile? ProfileImage { get; set; }
+}
