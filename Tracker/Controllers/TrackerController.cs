4e8c0bdbf2f4de434c428a71cb52a3c09c2e0888
bader <balqallaf12@gmail.com>
2025-02-04 00:28:27 +0300

    add expenses

--- Tracker/Controllers/TrackerController.cs
+++ Tracker/Controllers/TrackerController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Tracker.Data;
+using Tracker.Models;
+using Tracker.Models.ViewModels;
 
 namespace Tracker.Controllers
 {
     public class TrackerController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+
+        private SignInManager<ApplicationUser> signInManager;
+        private ApplicationDbContext db;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public TrackerController(UserManager<ApplicationUser> _userManager, 
+            SignInManager<ApplicationUser> _signInManager, 
+            IWebHostEnvironment webHostEnvironment, 
+            ApplicationDbContext _db)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            db = _db;
+            this.webHostEnvironment = webHostEnvironment;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult AddExpense()
+        {
+          //  ViewBag.Depts = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExpense(Expense model)
+        {
+            if (ModelState.IsValid)
+            {
+                Expense expenses = new Expense()
+                {
+                    ExpenseAmount = model.ExpenseAmount,
+                    ExpenseDate = model.ExpenseDate,
+                    Curency = model.Curency,
+                    Recurring = model.Recurring,
+                };
+                db.Expenses.Add(expenses);
+                db.SaveChanges();
+                return RedirectToAction("Home", "Index");
+            }
+       //     ViewBag.Depts = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            return View(model);
+        }
+
     }
 }
---
+++ Tracker/Models/ViewModels/ExpenseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using static Tracker.Models.ViewModels.EnumsList;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tracker.Models.ViewModels
+{
+    public class ExpenseViewModel
+    {
+        public Guid ExpenseId { get; set; }
+
+        [Required(ErrorMessage = "Enter The Amount")]
+        public decimal ExpenseAmount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+
+        [Required(ErrorMessage = "Choose a currency")]
+        public Currencies Curency { get; set; }
+        public bool? Recurring { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public int UserId { get; set; }
+        [ForeignKey("Budget")]
+        public int BudgetId { get; set; }
+
+    }
+}
